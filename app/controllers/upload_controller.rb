8980a64b5828ed1090b6e5ece688d67aa4db5249
Kevin Shu <Kevin@KevintekiMacBook-Pro.local>
2013-03-14 16:41:51 +0800

    m

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -28,26 +28,53 @@ class UploadController < ApplicationController
   end
   
   def oauth2callback
-    puts  'https://accounts.google.com/o/oauth2/token?'+
-          'code='+Rack::Utils.escape(params['code'])+
-          '&client_id=676069575889.apps.googleusercontent.com'+
-          '&client_secret=omQ28jfzJe_aziZU7p4UNctm'+
-          '&redirect_uri='+Rack::Utils.escape('http://localhost:3000/oauth2callback')+
-          '&grant_type=authorization_code',
-          :content_type => 'application/x-www-form-urlencoded'
-    response = RestClient.post  'https://accounts.google.com/o/oauth2/token?'+
-                                'code='+Rack::Utils.escape(params['code'])+
-                                '&client_id=676069575889.apps.googleusercontent.com'+
-                                '&client_secret=omQ28jfzJe_aziZU7p4UNctm'+
-                                '&redirect_uri='+Rack::Utils.escape('http://localhost:3000/upload/oauth2callback')+
-                                '&grant_type=authorization_code',
-                                :content_type => 'application/x-www-form-urlencoded'
     
-    puts '====================='
-    puts response[:access_token]
-    puts '====================='
+    require 'rubygems'
+    require 'faraday'
     
-    render :json => response.to_json
+    conn = Faraday.new(:url => 'https://accounts.google.com',:ssl => {:verify => false}) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+    
+    result = conn.post  '/o/oauth2/token', 
+                        { 'code' => Rack::Utils.escape(params['code']),
+                          'client_id' => "676069575889.apps.googleusercontent.com",
+                          'client_secret' => "Xd7h74h8f5V2XeqjMAhbEi1u",
+                          'redirect_uri' => Rack::Utils.escape("http://localhost:3000/upload/oauth2callback"),
+                          'grant_type' => 'authorization_code'
+                        }
+    
+    puts result.body.inspect
+    
+    client = YouTubeIt::OAuth2Client.new( :client_access_token => "access_token",
+                                          :client_refresh_token => "refresh_token", 
+                                          :client_id => "client_id", 
+                                          :client_secret => "client_secret", 
+                                          :dev_key => "dev_key", 
+                                          :expires_at => "expiration time")
+    
+    # puts  'https://accounts.google.com/o/oauth2/token?'+
+          # 'code='+Rack::Utils.escape(params['code'])+
+          # '&client_id=676069575889.apps.googleusercontent.com'+
+          # '&client_secret=omQ28jfzJe_aziZU7p4UNctm'+
+          # '&redirect_uri='+Rack::Utils.escape('http://localhost:3000/oauth2callback')+
+          # '&grant_type=authorization_code',
+          # :content_type => 'application/x-www-form-urlencoded'
+    # response = RestClient.post  'https://accounts.google.com/o/oauth2/token?'+
+                                # 'code='+Rack::Utils.escape(params['code'])+
+                                # '&client_id=676069575889.apps.googleusercontent.com'+
+                                # '&client_secret=omQ28jfzJe_aziZU7p4UNctm'+
+                                # '&redirect_uri='+Rack::Utils.escape('http://localhost:3000/upload/oauth2callback')+
+                                # '&grant_type=authorization_code',
+                                # :content_type => 'application/x-www-form-urlencoded'
+#     
+    # puts '====================='
+    # puts response[:access_token]
+    # puts '====================='
+    
+    render :json => result
     
   end
 end
